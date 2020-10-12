2e6c3a409e8660f31ac45c194dcd481309111400
Fedir <timosek198@gmail.com>
2020-10-12 23:05:55 +0300

    first commit

---
+++ For/For/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        For24();
+    }
+
+    static void For4()
+    {
+        int cena = 3, i = cena;
+        for (int x = 1; x <= 10; x++)
+        {
+            Console.WriteLine(cena);
+            cena += i;
+        }
+    }
+
+    static void For8()
+    {
+        int A = 3, B = 18, P = 1;
+        for (int i = A; i <= B; i++)
+            P *= i;
+        Console.WriteLine(P);
+    }
+
+    static void For12()
+    {
+        int N = 8;
+        double P = 1, x = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            x = x + 0.1;
+            P = P * x;
+        }
+        Console.WriteLine(P);
+    }
+
+    static void For16()
+    {
+        int A = 3, N = 14;
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(Math.Pow(A, i));
+        }
+    }
+
+    static void For20()
+    {
+        int N = 7, x = 0, fact = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            for (int u = 1; u <= i; u++)
+                fact = fact * u;
+            x = x + fact;
+            fact = 1;
+        }
+        Console.WriteLine(x);
+    }
+
+    static void For24()
+    {
+        int n = 3, z = 1;
+        double x = 5, x1 = 1, fact = 1, sum = 1;
+        for (int i = 1; i <= 2 * n; ++i)
+        {
+            fact *= i;
+            if (i % 2 == 0)
+            {
+                x1 = x * x1;
+                z = (-z);
+                sum += z * ((x1 * x1) / fact);
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ while/while/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        While24();
+    }
+
+    static void While4()
+    {
+        int N = 15;
+        while (N >= 3 && N % 3 == 0)
+            N = N / 3;
+        if (N == 1)
+            Console.Write("TRUE");
+        else
+            Console.Write("FALSE");
+    }
+
+    static void While8()
+    {
+        int N = 16, k = 0;
+        while (Math.Pow(k, 2) <= N)
+            k++;
+        Console.Write(k - 1);
+    }
+
+    static void While12()
+    {
+        int N = 16, k = 0, sum = 0;
+        while (sum <= N)
+        {
+            k++;
+            sum = sum + k;
+        }
+        Console.WriteLine(k - 1);
+        Console.WriteLine(sum - k);
+    }
+
+    static void While16()
+    {
+        int N = 10, K = 1, sum = 10, P = 10;
+        while (sum <= 200)
+        {
+            K++;
+            N = N + N / 100 * P;
+            sum = sum + N;
+        }
+        Console.WriteLine(K - 1);
+        Console.WriteLine(sum - N);
+    }
+
+    static void While20()
+    {
+        int N = 76527865, k = 0;
+        while (N >= 10)
+        {
+            k = N % 10;
+            if (k == 2)
+            {
+                Console.WriteLine("TRUE");
+                break;
+            }
+            N = N / 10;
+        }
+        if (k == 2)
+            Console.WriteLine("TRUE");
+        else
+            Console.WriteLine("FALSE");
+    }
+
+    static void While24()
+    {
+        int N = 21, sum = 0, i = 1, j = 1, j1 = 0;
+        while (i <= N)
+        {
+            i += j;
+            j1 = j;
+            j = i - j;
+        }
+        if (N == i - j1)
+            Console.WriteLine("TRUE");
+        else
+            Console.WriteLine("FALSE");
+    }
+
+}
